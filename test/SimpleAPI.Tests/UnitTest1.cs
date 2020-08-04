a7bde8d4a60b0cbfac3f234bbfc0aadf99fe3cd2
Shraddheya <shraddheya.shrivastava@gamil.com>
2020-08-04 15:17:30 +0530

    initial commit

---
+++ test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,43 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Tests
+{
+    public class UnitTest1
+    {
+        WeatherForecastController controller = new WeatherForecastController();
+
+        [Fact]
+        public void GetReturnsWeather()
+        {
+            int count = 0;
+            var returnvalue = controller.Get();
+            foreach (var item in returnvalue)
+            {
+                count++;
+            }
+            Assert.Equal(5, count);
+        }
+
+        [Fact]
+        public void GetSpecific()
+        {
+            var rns = (new Random()).Next(20);
+            int count = 0;
+            var returnvalue = controller.Get(rns);
+            foreach (var item in returnvalue)
+            {
+                count++;
+            }
+            Assert.Equal(rns, count);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+
+    }
+}
